060b0d81c018808be909ccdad7ee3840b803ca78
Alex Vann <ajevann@gmail.com>
2014-03-12 15:52:17 -0400

    Almost done. JS has been separated into separate methods.

--- javascripts/specific.js
+++ javascripts/specific.js
@@ -2,28 +2,29 @@
 var n = 20;
 $(".note").each(function() {
 	var hz = Math.pow(Math.pow(2, 1/12),n - 49) * 440;
-	$(this).text(Math.round(hz));
+	//$(this).text(Math.round(hz));
 	$(this).attr("id", Math.round(hz));
+
+	// var color = $(this).attr("id");
+	// color.length == 2 ? color = "0" + color: "";
+	// color.length == 4 ? color = color.substring(1,4): "";
+	// $(this).css("backgroundColor", "#" + color + "" + color);
+
 	n++;
 });
 
 $(".note").click(function() { 
 
-	if ($(this).attr("id") == "440")
+	if ($(this).attr("id") == "392")
 		animateTheme();	
 	else
 	{
-		playNote($(this).attr("id"));
-		animateNote($(this).attr("id"));
+		playNote($(this));
+		animateNote($(this));
 	}
+});
 
-
-	var color = $(this).text();
-	color.length == 2 ? color = "0" + color: "";
-	color.length == 4 ? color = color.substring(1,4): "";
-
-	$(this).animate({backgroundColor: "#" + color + "" + "000" + " !important;"}, "slow");
-
+function playNote(note) {
 	var data = []; // just an array
 
 	var hz = $(this).text();
@@ -37,33 +38,49 @@ $(".note").click(function() {
 	var wave = new RIFFWAVE(data); // create the wave file
 	var audio = new Audio(wave.dataURI); // create the HTML5 audio element
 	audio.play(); // some noise
-
-	var element = $(this);
-
-	setTimeout(function() {
-		element.animate({backgroundColor: "gray"}, "slow");
-	}, 2000);
-});
-
-function animateTheme() {
-	
-	
-	$("#392").animate({backgroundColor: "#" + $("#392").attr("id") + $("#392").attr("id")}, "slow");
-	$("#440").animate({backgroundColor: "#" + $("#440").attr("id") + $("#440").attr("id")}, "slow");
-	$("#349").animate({backgroundColor: "#" + $("#349").attr("id") + $("#349").attr("id")}, "slow");
-	$("#175").animate({backgroundColor: "#" + $("#175").attr("id") + $("#175").attr("id")}, "slow");
-	$("#262").animate({backgroundColor: "#" + $("#262").attr("id") + $("#262").attr("id")}, "slow");
-}
-
-function playNote(note) {
-	console.log("play");
 }
 
 function animateNote(note) {
-	console.log("animate");
+
+	var color = note.attr("id");
+	color.length == 2 ? color = "0" + color: "";
+	color.length == 4 ? color = color.substring(1,4): "";
+
+	note.animate({backgroundColor: "#" + color + "" + "000" + " !important;"}, "slow");
+
+	setTimeout(function() {
+		note.animate({backgroundColor: "gray"}, "slow");
+	}, 2000);
 }
 
+function animateTheme() {
 
+	setTimeout(function() {
+		playNote($("#392"));
+		animateNote($("#392"));
+	
+		setTimeout(function() {
+			playNote($("#440"));
+			animateNote($("#440"));
+
+			setTimeout(function() {
+				playNote($("#349"));
+				animateNote($("#349"));
+			
+				setTimeout(function() {
+					playNote($("#175"));
+					animateNote($("#175"));
+				
+					setTimeout(function() {
+						playNote($("#262"));
+						animateNote($("#262"));
+					}, 2000);
+				}, 2000);
+			}, 2000);
+		}, 2000);
+	}, 2000);
+
+}
 
 
 

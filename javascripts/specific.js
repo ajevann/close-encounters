c036f270acb805d51daba2ce765b48b58c517ede
Alex Vann <ajevann@gmail.com>
2014-03-12 20:44:14 -0400

    Change frequency generator to something that made sense (oscillator). Added "startShow()". Added theme detector.

--- javascripts/specific.js
+++ javascripts/specific.js
@@ -1,91 +1,141 @@
+$(document).ready(function() {
 
-var n = 20;
-$(".note").each(function() {
-	var hz = Math.pow(Math.pow(2, 1/12),n - 49) * 440;
-	//$(this).text(Math.round(hz));
-	$(this).attr("id", Math.round(hz));
+	(function init(g){
+		try {
+			audio_context = new (g.AudioContext || g.webkitAudioContext);
+			oscillator = audio_context.createOscillator();
+		} catch (e) {
+			alert('No web audio oscillator support in this browser');
+		}
+	}(window));
 
-	// var color = $(this).attr("id");
-	// color.length == 2 ? color = "0" + color: "";
-	// color.length == 4 ? color = color.substring(1,4): "";
-	// $(this).css("backgroundColor", "#" + color + "" + color);
+	var context = new webkitAudioContext();
+	var oscillator = context.createOscillator();
 
-	n++;
-});
+	var lastNotes = [];
+	var themeNotes = ["392", "440", "349", "175", "262"];
 
-$(".note").click(function() { 
+	var notes = [];
 
-	if ($(this).attr("id") == "392")
-		animateTheme();	
-	else
-	{
-		playNote($(this));
-		animateNote($(this));
-	}
-});
+	var n = 20;
+	var i = 0;
+	$(".note").each(function() {
+		var hz = Math.pow(Math.pow(2, 1/12),n - 49) * 440;
+		//$(this).text(Math.round(hz));
+		$(this).attr("id", Math.round(hz));
+		notes[i++] = Math.round(hz).toString();
 
-function playNote(note) {
-	var data = []; // just an array
+		// var color = $(this).attr("id");
+		// color.length == 2 ? color = "0" + color: "";
+		// color.length == 4 ? color = color.substring(1,4): "";
+		// $(this).css("backgroundColor", "#" + color + "" + color);
 
-	var hz = $(this).text();
+		n++;
+	});
 
-	for (var i=0; i<1000; i++)  // size of i's limit determines the length of the sound
-	{	
-		data[i] = 128+Math.round(127*Math.sin(i * hz)); // fill data with random samples
-	}
-	//880 - A note
-	
-	var wave = new RIFFWAVE(data); // create the wave file
-	var audio = new Audio(wave.dataURI); // create the HTML5 audio element
-	audio.play(); // some noise
-}
+	$(".note").click(function() { 
 
-function animateNote(note) {
+		console.log(notes);
 
-	var color = note.attr("id");
-	color.length == 2 ? color = "0" + color: "";
-	color.length == 4 ? color = color.substring(1,4): "";
+		startShow();
 
-	note.animate({backgroundColor: "#" + color + "" + "000" + " !important;"}, "slow");
+		push($(this));
 
-	setTimeout(function() {
-		note.animate({backgroundColor: "gray"}, "slow");
-	}, 2000);
-}
-
-function animateTheme() {
-
-	setTimeout(function() {
-		playNote($("#392"));
-		animateNote($("#392"));
-	
+		playNote($(this), 1000);
+		animateNote($(this), 1000);
+		
 		setTimeout(function() {
-			playNote($("#440"));
-			animateNote($("#440"));
+			if (lastNotes.toString() == themeNotes.toString())
+				animateTheme(750);
+		}, 2000);	
+
+		console.log(themeNotes);
+		console.log(lastNotes);
+	});
+
+	function push(note) {
+		lastNotes[0] = lastNotes[1];
+		lastNotes[1] = lastNotes[2];
+		lastNotes[2] = lastNotes[3];
+		lastNotes[3] = lastNotes[4];
+
+		lastNotes[4] = note.attr("id").toString();
+	}
+
+	function playNote(note, interval) {
+
+	    oscillator.type = 0;
+	    oscillator.frequency.value = note.attr("id") * 2;
+	    oscillator.connect(context.destination);
+	    oscillator.noteOn && oscillator.noteOn(0); // this method doesn't seem to exist, though it's in the docs?
+
+		setTimeout(function() {
+	    	oscillator.disconnect(); 
+			
+			oscillator = context.createOscillator();
+	    }, interval);
+	}
+
+	function animateNote(note, interval) {
+
+		var color = note.attr("id");
+		color.length == 2 ? color = "0" + color: "";
+		color.length == 4 ? color = color.substring(1,4): "";
+
+		note.animate({backgroundColor: "#" + color + "" + "000" + " !important;"}, "slow");
+
+		setTimeout(function() {
+			note.animate({backgroundColor: "gray"}, "slow");
+		}, interval + 250);
+	}
+
+	function startShow() {
+		for (var i = 0; i < 10; i++) {
+			var n = Math.floor((Math.random()*60));
+			var na = notes[n];
+			var nc = $("#" + na);
+
+			console.log(n + " " + na + " " + nc);
+
+			animateNote(nc, 100000);
+		}
+	}
+
+	function animateTheme(interval) {
+
+		var q = Math.round(interval);
+		var w = 4 * q;
+
+		console.log(q);
+
+
+		playNote($("#392"), q);
+		animateNote($("#392"), q);
+
+		setTimeout(function() {
+			playNote($("#440"), q);
+			animateNote($("#440"), q);
 
 			setTimeout(function() {
-				playNote($("#349"));
-				animateNote($("#349"));
+				playNote($("#349"), q);
+				animateNote($("#349"), q);
 			
 				setTimeout(function() {
-					playNote($("#175"));
-					animateNote($("#175"));
+					playNote($("#175"), q);
+					animateNote($("#175"), q);
 				
 					setTimeout(function() {
-						playNote($("#262"));
-						animateNote($("#262"));
-					}, 2000);
-				}, 2000);
-			}, 2000);
-		}, 2000);
-	}, 2000);
+						playNote($("#262"), w);
+						animateNote($("#262"), w);
 
-}
+						setTimeout(function() {
+							animateTheme(interval * .75);
+						}, w + 100);
 
+					}, q + 100);
+				}, q + 100);
+			}, q + 100);
+		}, q + 100);
+	}
 
-
-
-
-
-
-
+});

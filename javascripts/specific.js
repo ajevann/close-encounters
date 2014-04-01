920f93334ffe21dc6698d8202f4bb02905a3c55a
Alex Vann <ajevann@gmail.com>
2014-04-01 17:27:37 -0400

    Changed a lot. Fixed fading with riffwave.js, corrected color scheme (to-do: added color fades by row), fixed note order to correspond how it looks in the movie.

--- javascripts/specific.js
+++ javascripts/specific.js
@@ -13,29 +13,41 @@ $(document).ready(function() {
 		oscillator = context.createOscillator();
 
 	var lastNotes = [];
-	var themeNotes = ["392", "440", "349", "175", "262"];
+	var themeNotes = ["33", "35", "31", "50", "5"];
 
 	var notes = [];
 
-	var n = 20;
+	var n = 35;
+	var m = 31;
 	var i = 0;
+	var count = 0;
 	$(".note").each(function() {
 		var hz = Math.pow(Math.pow(2, 1/12),n - 49) * 440;
-		//$(this).text(Math.round(hz));
-		$(this).attr("id", Math.round(hz));
-		notes[i++] = Math.round(hz).toString();
+		//hz = Math.pow(Math.pow(2,1/12),m) * 440;
 
+		$(this).text(Math.round(hz) + " (" + i + ")");
+		$(this).attr("id", i);
+		$(this).attr("data-tone", Math.round(hz));
+		$(this).attr("data-color", setColor(i));
+
+		notes[i++] = Math.round(hz).toString();
+		
 		// var color = $(this).attr("id");
 		// color.length == 2 ? color = "0" + color: "";
 		// color.length == 4 ? color = color.substring(1,4): "";
 		// $(this).css("backgroundColor", "#" + color + "" + color);
 
 		n++;
+		m--;
+		if (i%24 == 0) { n = n - 21; }
+		if (i%48 == 0) { n = n - 10; }
 	});
 
 	$(".note").click(function() { 
 
-		//startShow();
+		startShow();
+
+		console.log($(this).attr("id") % 12 + "  " + Math.floor($(this).attr("id") / 12));
 
 		push($(this));
 
@@ -48,8 +60,64 @@ $(document).ready(function() {
 		}, 2000);	
 
 		// console.log(themeNotes);
-		// console.log(lastNotes);
+		console.log(lastNotes);
 	});
+
+	//Source: http://js.do/blog/sound-waves-with-javascript/
+	function playNote(note, interval) {
+
+		var sampleRate = 44.1 * interval;
+		//  var samples_length = sampleRate; // divide by 2 ???
+		var samples = [] //new Float32Array(samples_length);
+
+		var frequency = note.attr("data-tone") * 2;
+		var samples_length = 44.1 * interval;               // Plays for 1 second (44.1 KHz)
+		for (var i=0; i < samples_length ; i++) { // fills array with samples
+			var t = i/samples_length;               // time from 0 to 1
+			samples[i] = Math.sin( frequency * 2*Math.PI*t ); // wave equation (between -1,+1)
+			samples[i] *= (1-t);
+		}
+
+		if (samples.length==0) {
+			alert("ERROR: No values in array 'samples'");
+			return;
+		}
+
+		normalize_invalid_values(samples); // keep samples between [-1, +1]
+
+		//draw_canvas_graph(samples);
+
+		var wave = new RIFFWAVE();
+		wave.header.sampleRate = sampleRate;
+		wave.header.numChannels = 1;
+		var audio = new Audio();
+		var samples2=convert255(samples);
+		wave.Make(samples2);
+		audio.src=wave.dataURI;
+		setTimeout(function() { audio.play(); }, 10); // page needs time to load?
+
+	}
+
+	//Source: http://js.do/blog/sound-waves-with-javascript/
+	function convert255(data) {
+		var data_0_255=[];
+		for (var i=0;i<data.length;i++) {
+			data_0_255[i]=128+Math.round(127*data[i]);
+		}
+		return data_0_255;
+	}
+
+	//Source: http://js.do/blog/sound-waves-with-javascript/
+	function normalize_invalid_values(samples) {
+		for (var i=0, len=samples.length; i<len; i++) {
+			if (samples[i]>1) {
+				samples[i] = 1;
+			} else if (samples[i]<-1) {
+				samples[i] = -1;
+			}
+		}
+	}
+
 
 	function push(note) {
 		lastNotes[0] = lastNotes[1];
@@ -60,29 +128,12 @@ $(document).ready(function() {
 		lastNotes[4] = note.attr("id").toString();
 	}
 
-	function playNote(note, interval) {
-    	oscillator.disconnect(); 
-		oscillator = context.createOscillator();
-
-	    oscillator.type = 0;
-	    oscillator.frequency.value = note.attr("id") * 2;
-	    oscillator.connect(context.destination);
-
-	    oscillator.noteOn && oscillator.noteOn(0); // turns on the sound. this method doesn't seem to exist, though it's in the docs?
-
-		setTimeout(function() {
-	    	oscillator.disconnect(); 
-			oscillator = context.createOscillator();
-	    }, interval);
-	}
-
 	function animateNote(note, interval) {
 
-		var color = note.attr("id");
-		color.length == 2 ? color = "0" + color: "";
-		color.length == 4 ? color = color.substring(1,4): "";
+		var color = note.attr("data-color");
+		console.log(color);
 
-		note.animate({backgroundColor: "#" + color + "" + "000" + " !important;"}, "slow");
+		note.animate({backgroundColor: "#" + color + " !important;"}, "slow");
 
 		setTimeout(function() {
 			note.animate({backgroundColor: "gray"}, "slow");
@@ -91,56 +142,83 @@ $(document).ready(function() {
 
 	function startShow() {
 
-		for (var i = 0; i < 25; i++) {
-
-			var n = Math.floor((Math.random()*60));
+		for (var i = 0; i < 15; i++) {
+			console.log(i);
+			var n = Math.floor((Math.random()*72));
 			var na = notes[n];
-			var nc = $("#" + na);
 
 			console.log(n + " " + na);
 
-			animateNote(nc, 500);
+			animateNote($("#" + n), 500);
 		}
 	}
 
 	function animateTheme(interval) {
 
-		var q = Math.round(interval);
-		var w = 4 * q;
-
-		console.log(q);
-
-		playNote($("#392"), q);
-		animateNote($("#392"), q);
-
-		setTimeout(function() {
-			playNote($("#440"), q);
-			animateNote($("#440"), q);
-
-			setTimeout(function() {
-				playNote($("#349"), q);
-				animateNote($("#349"), q);
-			
-				setTimeout(function() {
-					playNote($("#175"), q);
-					animateNote($("#175"), q);
-				
-					setTimeout(function() {
-						playNote($("#262"), w);
-						animateNote($("#262"), w);
-
-						setTimeout(function() {
-							if (q == 100) {
-								animateTheme(100);
-								startShow(); 
-							} 
-							else
-								animateTheme(interval * .75);
-						}, w + 100);
-					}, q + 100);
-				}, q + 100);
-			}, q + 100);
-		}, q + 100);
 	}
 
+	function setColor(id)
+	{
+		//Column - %
+		var col = id % 12;
+
+		//Row - floor(/)
+		var row = Math.floor(id / 12);
+
+		var color = "";
+
+		switch (col) {
+			case 0:		color = "FFFF00";		break;
+			case 1:		color = "80FF00";		break;
+			case 2:		color = "00FF00";		break;
+			case 3:		color = "00FF80";		break;
+			case 4:		color = "00FFFF";		break;
+			case 5:		color = "0080FF";		break;
+			case 6:		color = "0000FF";		break;
+			case 7:		color = "8000FF";		break;
+			case 8:		color = "FF00FF";		break;
+			case 9:		color = "FF0080";		break;
+			case 10:	color = "FF0000";		break;
+			case 11:	color = "FF8000";		break;
+		}
+
+
+
+
+		return color;
+
+	}
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
